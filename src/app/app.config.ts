6ab0ab4eb2c183a6f3172d07511d65f6c76ff2c3
fayeyoussou <fayeyoussoupha@gmail.com>
2024-02-09 12:52:05 +0000

    changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { environment} from "../environment/environment";
 import { routes } from './app.routes';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -8,8 +8,6 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {environment} from "../environment/environment";
-
 import {AngularFireModule} from "@angular/fire/compat";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 import {AngularFireAuthModule} from "@angular/fire/compat/auth";
@@ -21,6 +19,5 @@ export const appConfig: ApplicationConfig = {
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
-
   ]), importProvidersFrom(HttpClientModule), provideAnimations()]
 };
