4d80049fc623e3924a96a8c14e08bcd68aba9aa5
fayeyoussou <fayeyoussoupha@gmail.com>
2024-02-11 17:31:56 +0000

    new update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# MentalHealthApp
+# ThoughCloud
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.10.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.3.
 
 ## Development server
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,30 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { environment} from "../environment/environment";
-import { routes } from './app.routes';
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
+
+import {routes} from './app.routes';
+import {en_US, provideNzI18n} from 'ng-zorro-antd/i18n';
+import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import {AngularFireModule} from "@angular/fire/compat";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 
 registerLocaleData(en);
 
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule,[
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp({
+      "projectId": "though-cloud",
+      "appId": "1:299154598634:web:dace4946ef9b156412c593",
+      "storageBucket": "though-cloud.appspot.com",
+      "apiKey": "AIzaSyCioW-wY4NI9GU4BsI7QJFfCq-BUQTdHR8",
+      "authDomain": "though-cloud.firebaseapp.com",
+      "messagingSenderId": "299154598634"
+    }),
     AngularFirestoreModule,
     AngularFireAuthModule,
   ]), importProvidersFrom(HttpClientModule), provideAnimations()]
